390d1aeee3e479a06341918668bc6e36c14ba103
Greg <gmercer10@my.bcit.ca>
2018-09-26 21:11:38 -0700

    Wrote and performed successful unit tests for each function

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -2,13 +2,47 @@
 // Created by Greg on 2018-09-26.
 //
 
+#define SAMPLE_VALUES {2, 5, 12, 10, 1, 6, 23, 0, 92, 13}
+#define LAST_VALUE 13
+#define THIRD_LAST_VALUE 0
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() put this in one cpp file
 #include "catch.hpp"
 #include "stack.hpp"
 
-TEST_CASE("A new stack is empty", "[stack")
-{
+TEST_CASE("A new stack is empty", "[stack]") {
     stack tester;
     REQUIRE(tester.empty() == true);
     REQUIRE(tester.full() == false);
+}
+
+TEST_CASE("Fill stack", "[stack]") {
+
+    stack tester;
+    int values[] = SAMPLE_VALUES;
+
+    for(int i = 0; i < sizeof(values)/sizeof(*values); i++) {
+        tester.push(values[i]);
+    }
+
+    REQUIRE(tester.full() == true);
+    REQUIRE(tester.top() == LAST_VALUE);
+
+    tester.print();
+}
+
+TEST_CASE("Pop test", "[stack]") {
+
+    stack tester;
+    int values[] = SAMPLE_VALUES;
+
+    for(int i = 0; i < sizeof(values)/sizeof(*values); i++) {
+        tester.push(values[i]);
+    }
+
+    tester.pop();
+    tester.pop();
+
+    REQUIRE(tester.top() == THIRD_LAST_VALUE);
+
+    tester.print();
 }
