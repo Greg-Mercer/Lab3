81cf4442cb5d42071a1bc19ed98fb02d9e80e413
Greg <gmercer10@my.bcit.ca>
2018-09-26 20:04:21 -0700

    Added member functions

---
+++ stack.cpp
@@ -0,0 +1,62 @@
+//
+// Created by Greg on 2018-09-26.
+//
+
+#include <iostream>
+#include "stack.hpp"
+
+#define STACK_SIZE 10
+#define BEGIN 0
+#define INIT_INDEX -1
+
+using namespace std;
+
+class stack {
+
+    private:
+        int intStack[STACK_SIZE];
+        int topIndex;
+
+    public:
+        stack();
+        bool push(int value);
+        void pop();
+        const int top();
+        const bool empty();
+        const bool full();
+        const void print();
+};
+
+stack::stack() {
+    topIndex = INIT_INDEX;
+}
+
+bool stack::push(int value) {
+    if(topIndex != STACK_SIZE INIT_INDEX) { // How's that for not using magic numbers?
+        topIndex++;
+        intStack[topIndex] = value;
+        return true;
+    } else return false;
+}
+
+void stack::pop() {
+    topIndex--;
+}
+
+const int stack::top() {
+    return intStack[topIndex];
+}
+
+const bool stack::empty() {
+    return topIndex == -1;
+}
+
+const bool stack::full() {
+    return topIndex == STACK_SIZE INIT_INDEX;
+}
+
+const void stack::print() {
+    cout << "Stack contents:" << endl;
+    for(int i = BEGIN; i <= topIndex; i++)
+        cout << i << ": " << intStack[i] << endl;
+}
---
+++ stack.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Greg on 2018-09-26.
+//
+
+#ifndef LAB3_STACK_HPP
+#define LAB3_STACK_HPP
+
+#endif //LAB3_STACK_HPP
+
+void stack();
+bool push(int value);
+void pop();
+const int top();
+const bool empty();
+const bool full();
+const void print();
