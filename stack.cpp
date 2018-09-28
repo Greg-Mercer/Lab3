b92c25aee36866ac5c7ef6372f67569ce41db8d8
Greg <gmercer10@my.bcit.ca>
2018-09-27 17:05:33 -0700

    Added comments

--- stack.cpp
+++ stack.cpp
@@ -12,10 +12,18 @@
 
 using namespace std;
 
+/*
+ * Initializes a stack with index of first element pointing to nothing
+ */
 stack::stack() {
     topIndex = INIT_INDEX;
 }
 
+/*
+ * Pushes a value into the stack if there is room
+ * @param value the value to push
+ * @return true if value was pushed, else false
+ */
 bool stack::push(int value) {
     if(topIndex != STACK_SIZE INIT_INDEX) { // How's that for not using magic numbers?
         topIndex++;
@@ -24,25 +32,44 @@ bool stack::push(int value) {
     } else return false;
 }
 
+/*
+ * Removes the last value pushed into the stack
+ */
 void stack::pop() {
     topIndex--;
 }
 
+/*
+ * Returns the int stored at the top of the stack
+ * @return the int at the top of the stack
+ */
 const int stack::top() {
     const int top = intStack[topIndex];
     return top;
 }
 
+/*
+ * Tests if the stack is empty
+ * @return true if stack is empty, else false
+ */
 const bool stack::empty() {
     const bool isEmpty = topIndex == INIT_INDEX;
     return isEmpty;
 }
 
+/*
+ * Tests if the stack is full
+ * @return true if stack is full, else false
+ */
 const bool stack::full() {
     const bool isFull = topIndex == STACK_SIZE INIT_INDEX;
     return isFull;
 }
 
+/*
+ * Prints stack contents into a stringstream, then converts it into a string
+ * @return string representing stack contents
+ */
 const string stack::print() {
     stringstream stackPrt;
     stackPrt << "Stack contents:" << endl;
--- stack.hpp
+++ stack.hpp
@@ -8,18 +8,22 @@
 
 #endif //LAB3_STACK_HPP
 
+/*
+ * A last in, first out data structure comprised of an array of ints and an index pointing to
+ * the last element added to the stack. Contains methods to test and manipulate stack data.
+ */
 class stack {
 
-private:
-    int intStack[STACK_SIZE];
-    int topIndex;
+    private:
+        int intStack[STACK_SIZE];   // functions as the stack
+        int topIndex;               // index of the top element in the stack
 
-public:
-    stack();
-    bool push(int value);
-    void pop();
-    const int top();
-    const bool empty();
-    const bool full();
-    const std::string print();
+    public:
+        stack();
+        bool push(int value);
+        void pop();
+        const int top();
+        const bool empty();
+        const bool full();
+        const std::string print();
 };
